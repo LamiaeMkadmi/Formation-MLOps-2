d0ed4bfdadab496b91dfbc1e622e6cac3e78e86b
lmk <mkadmi-lamiae@outlook.fr>
2024-06-18 12:49:56 +0000

    mlflow added

--- formation_indus_ds_avancee/train_and_predict.py
+++ formation_indus_ds_avancee/train_and_predict.py
@@ -19,6 +19,8 @@ def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
     y = features[target]
     with mlflow.start_run():
         # insert autolog here ...
+        #mlflow.set_tracking_url("http://0.0.0.0:36039")
+        mlflow.sklearn.autolog()
         model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
         model.fit(X, y)
     time_str = time.strftime('%Y%m%d-%H%M%S')
